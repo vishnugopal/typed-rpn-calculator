305e7ad37eedab7a19d8965bd6395f6c4edcb396
Vishnu Gopal <vishnugopal@users.noreply.github.com>
2020-05-04 18:38:47 +0530

    Clear up everything

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,9 @@
-import React, { useReducer } from "react";
-import Display from "./components/Display";
+import React from "react";
 
 import "./styles.css";
-import DigitButton from "./components/DigitButton";
-import OperatorButton from "./components/OperatorButton";
-import EnterButton from "./components/EnterButton";
-
-import calculatorReducer from "./calculatorReducer";
-import { State } from "./types/Calculator";
 
 function App() {
-  const initialState: State = { buffer: "", stack: [] };
-  const [state, dispatch] = useReducer(calculatorReducer, initialState);
-
-  const digitButtonClick = (digit: number) => {
-    dispatch({ type: "digitPressed", payload: digit });
-  };
-
-  return (
-    <div className="max-w-md m-auto">
-      <Display buffer={state.buffer} />
-      <DigitButton digit={7} onClick={digitButtonClick} />
-      <DigitButton digit={8} onClick={digitButtonClick} />
-      <DigitButton digit={9} onClick={digitButtonClick} />
-      <OperatorButton operator="+" />
-      <br />
-      <DigitButton digit={4} onClick={digitButtonClick} />
-      <DigitButton digit={5} onClick={digitButtonClick} />
-      <DigitButton digit={6} onClick={digitButtonClick} />
-      <OperatorButton operator="-" />
-      <br />
-      <DigitButton digit={1} onClick={digitButtonClick} />
-      <DigitButton digit={2} onClick={digitButtonClick} />
-      <DigitButton digit={3} onClick={digitButtonClick} />
-      <EnterButton />
-    </div>
-  );
+  return <div className="max-w-md m-auto"></div>;
 }
 
 export default App;
--- src/tests/components/Display.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import Display from "../../components/Display";
-
-test("renders App", () => {
-  const { getByRole } = render(<Display buffer="123" />);
-  expect(getByRole("textbox")).toHaveTextContent("123");
-});
